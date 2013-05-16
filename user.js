15977d61cb7bcd0d54c1b2e3a8c4189073dd2984
Robin McCorkell <rmccorkell@karoshi.org.uk>
2013-05-16 11:26:46 +0100

    Initial commit

---
+++ user.js
@@ -0,0 +1,23 @@
+user_pref("mail.identity.id1.useremail", "rmccorkell08@dovergramboys.kent.sch.uk");
+user_pref("mail.identity.id1.valid", true);
+user_pref("mail.identity.id1.fullName", "Robin McCorkell");
+user_pref("mail.identity.id1.smtpServer", "smtp1");
+
+user_pref("mail.server.server1.check_new_mail", true);
+user_pref("mail.server.server1.hostname", "mail.dovergramboys.kent.sch.uk");
+user_pref("mail.server.server1.login_at_startup", true);
+user_pref("mail.server.server1.max_cached_connections", 5);
+user_pref("mail.server.server1.name", "rmccorkell08@dovergramboys.kent.sch.uk");
+user_pref("mail.server.server1.socketType", 2);
+user_pref("mail.server.server1.spamActionTargetAccount", "imap://rmccorkell08@mail.dovergramboys.kent.sch.uk");
+user_pref("mail.server.server1.storeContractID", "@mozilla.org/msgstore/berkeleystore;1");
+user_pref("mail.server.server1.timeout", 29);
+user_pref("mail.server.server1.type", "imap");
+user_pref("mail.server.server1.userName", "rmccorkell08");
+
+user_pref("mail.smtpserver.smtp1.authMethod", 3);
+user_pref("mail.smtpserver.smtp1.hostname", "mail.dovergramboys.kent.sch.uk");
+user_pref("mail.smtpserver.smtp1.port", 25);
+user_pref("mail.smtpserver.smtp1.try_ssl", 0);
+user_pref("mail.smtpserver.smtp1.username", "rmccorkell08");
+user_pref("mail.smtpservers", "smtp1");
