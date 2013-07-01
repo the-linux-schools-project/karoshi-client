ec42f96fd7e81932a9f1b929ef414fa9fe48c0d1
Robin McCorkell <rmccorkell@karoshi.org.uk>
2013-07-01 15:03:55 +0100

    Fix getting error status from apt-get inside subshell

--- install.sh
+++ install.sh
@@ -114,7 +114,7 @@ if [[ -f install/install-list ]]; then
 	packages=( $(< install/install-list) )
 	#This is a bit of a hack to get error output stored in a variable as well as output
 	exec 11>&1
-	rm_packages=$(apt-get -qy --allow-unauthenticated install ${packages[@]} 2>&1 >&11 | tee /dev/fd/2 | sed -n 's/^E: Unable to locate package //p')
+	rm_packages=$(apt-get -qy --allow-unauthenticated install ${packages[@]} 2>&1 >&11 | tee /dev/fd/2 | sed -n 's/^E: Unable to locate package //p'; exit ${PIPESTATUS[0]})
 	err=$?
 	exec 11>&-
 	if [[ $err -eq 100 ]]; then
